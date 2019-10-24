dbeaaad2152d2d5b453c2ca02a39d3cc2e7c78c3
Leslie Meng <lmeng4@bu.edu>
2019-10-24 19:59:02 -0400

    Added comments to classes that defines methods and variables

--- blokus_core.py
+++ blokus_core.py
@@ -6,7 +6,32 @@ import json
 
 class Piece:
     '''
-    A single game piece.
+    A single game piece. 
+
+    Methods:
+    get_edge_squares()
+        - returns a list of edges of this piece, only for init
+    get_corner_squares() 
+        - returns a list of corners of this piece, only for init
+    square_locations()
+        - returns a list of where the squares would be based on an origin
+    edge_locations()
+        - square_locations() but for edges
+    corner_locations()
+        - square_locations() but for corners
+
+    Variables:
+    color       - The color of the piece: red, blue, yellow, or green
+    value       - The point value of the piece and the amount of squares it has
+
+    squares     - List of location of all squares relative to an arbitrary 
+    corners     - List of location of all squares that would be on a corner
+    edges       - List of location of all squares that would be on an edge
+                    origin square location 
+    rotation    - Value from 0-3. An increase of 1 in this value corresponds 
+                    with a 90-degree clockwise rotation
+    flipped     - True if flipped. Always flipped over y-axis
+
     '''
     def __init__(self, color='blue', squares=[[0, 0]], rotation=0, flipped=False):
         self.color = color
@@ -23,6 +48,15 @@ class Piece:
 class Player:
     '''
     Represents a single player. Used to initialize all the pieces as well.
+    
+    Methods:
+    get_legal_moves(board)
+        - returns a list of all legal moves, used by choose_move()
+
+    Variables:
+    color       - blue, yellow, red, green
+    score       - a tally of the player's score
+    pieces      - a list of all pieces still in the player's hand
     '''
     def __init__(self, color):
         self.color = color
@@ -40,6 +74,15 @@ class Player:
 class Board:
     '''
     Represents the game board. Holds a 2d array and associated methods
+
+    Methods:
+    is_legal_move(self, piece, origin)
+        - Returns True if move is legal. Accounts for corners, adjacency, and overlap
+
+    Variables:
+    spaces      - 2d array that holds pieces
+    corners     - 2d array that holds lists of colors that can play for each square
+    edges       - 2d array that holds lists of colors that cannot play for each square
     '''
 
     def __init__(self, num_players=4, length=20, height=20):
